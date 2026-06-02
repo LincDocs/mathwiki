76f8287991f106ee9d93d4ba157cf6046d90f561
LincZero <LincZero@users.noreply.github.com>
2026-06-02 14:35:22 +0000

    Deploying to gh-pages from @ LincDocs/mathwiki@4e11d10462ebafadf05f95e6fd05c8a9c0e18e9c 🚀

---
+++ assets/references.json.html-jnn5Uxw_.js
@@ -0,0 +1,75 @@
+import{_ as o,c as e,a as n,r as u,o as r}from"./app-DY6QWHc_.js";const a={};function q(i,s){const t=u("VueFlow");return r(),e("div",null,[n(t,{type:"nodeflow-comfyui",data:`[
+    {"Mac97":
+        {
+            "title": "Categories for the Working Mathematician",
+            "author": ["Saunders Mac Lane"],
+            "date": "1997",
+            "edition": "2",
+            "series": "Graduate Texts in Mathematics",
+            "publisher": "Springer New York",
+            "link": "https://doi.org/10.1007/978-1-4757-4721-8",
+            "type": "book",
+            "label": "Mac97"
+        }
+    },
+    {"Alu09":
+        {
+            "title": "Algebra: Chapter 0",
+            "author": ["Paulo Allufi"],
+            "date": "2009",
+            "series": "Graduate Studies in Mathematics",
+            "volume": "104",
+            "publisher": "American Mathematical Society",
+            "link": "https://bookstore.ams.org/gsm-104/",
+            "type": "book",
+            "label": "Alu09"
+        }
+    },
+    {"Tse22":
+        {
+            "title": "Topics in Ergodic Theory and Measured Group Theory",
+            "author": ["Anush Tserunyan"],
+            "date": "2022",
+            "link": "https://www.math.mcgill.ca/atserunyan/Courses/2022_W.Math594.Erg&MsGrp/",
+            "type": "online",
+            "label": "Tse22"
+        }
+    },
+    {"Tse23":
+        {
+            "title": "Advanced Real Analysis 1",
+            "author": ["Anush Tserunyan"],
+            "date": "2023",
+            "link": "https://www.math.mcgill.ca/atserunyan/Courses/2023F.Math564.Analysis%201/",
+            "type": "online",
+            "label": "Tse23"
+        }
+    },
+    {"Fol99":
+        {
+            "title": "Real Analysis, Modern Techniques and Their Applications",
+            "author": ["Gerald B. Folland"],
+            "date": "1999",
+            "edition": "2",
+            "series": "Pure and Applied Mathematics",
+            "publisher": "A Wiley Series of Texts, Monographs and Tracts, John Wiley & Sons",
+            "link": "https://www.wiley.com/en-us/Real+Analysis%3A+Modern+Techniques+and+Their+Applications%2C+2nd+Edition-p-9780471317166",
+            "type": "book",
+            "label": "Fol99"
+        }
+    },
+    {"Fur97":
+        {
+            "title": "Ergodic behavior of diagonal measures and a theorem of Szemerédi on arithmetic progressions",
+            "author": ["Harry Furstenberg"],
+            "date": "1977",
+            "journal": "Journal d’Analyse Mathématique",
+            "volume": "31",
+            "pages": "204-256",
+            "type": "article",
+            "label": "Fur97"
+        }
+    }
+]
+
+`})])}const h=o(a,[["render",q]]),b=JSON.parse('{"path":"/references.json.html","title":"references.json","lang":"zh-CN","frontmatter":{"description":"[ {&quot;Mac97&quot;: { &quot;title&quot;: &quot;Categories for the Working Mathematician&quot;, &quot;author&quot;: [&quot;Saunders Mac Lane&quot;], &quot;date&quot;: &quot;199...","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":null,\\"author\\":[]}"],["meta",{"property":"og:url","content":"https://LincDocs.github.io/mathwiki/references.json.html"}],["meta",{"property":"og:site_name","content":"mathwiki"}],["meta",{"property":"og:description","content":"[ {&quot;Mac97&quot;: { &quot;title&quot;: &quot;Categories for the Working Mathematician&quot;, &quot;author&quot;: [&quot;Saunders Mac Lane&quot;], &quot;date&quot;: &quot;199..."}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}]],"layout":"Layout"},"git":{},"readingTime":{"minutes":0.65,"words":194},"filePathRelative":"references.json","excerpt":"<p>[<br>\\n{&quot;Mac97&quot;:<br>\\n{<br>\\n&quot;title&quot;: &quot;Categories for the Working Mathematician&quot;,<br>\\n&quot;author&quot;: [&quot;Saunders Mac Lane&quot;],<br>\\n&quot;date&quot;: &quot;1997&quot;,<br>\\n&quot;edition&quot;: &quot;2&quot;,<br>\\n&quot;series&quot;: &quot;Graduate Texts in Mathematics&quot;,<br>\\n&quot;publisher&quot;: &quot;Springer New York&quot;,<br>\\n&quot;link&quot;: &quot;<a href=\\"https://doi.org/10.1007/978-1-4757-4721-8\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">https://doi.org/10.1007/978-1-4757-4721-8</a>&quot;,<br>\\n&quot;type&quot;: &quot;book&quot;,<br>\\n&quot;label&quot;: &quot;Mac97&quot;<br>\\n}<br>\\n},<br>\\n{&quot;Alu09&quot;:<br>\\n{<br>\\n&quot;title&quot;: &quot;Algebra: Chapter 0&quot;,<br>\\n&quot;author&quot;: [&quot;Paulo Allufi&quot;],<br>\\n&quot;date&quot;: &quot;2009&quot;,<br>\\n&quot;series&quot;: &quot;Graduate Studies in Mathematics&quot;,<br>\\n&quot;volume&quot;: &quot;104&quot;,<br>\\n&quot;publisher&quot;: &quot;American Mathematical Society&quot;,<br>\\n&quot;link&quot;: &quot;<a href=\\"https://bookstore.ams.org/gsm-104/\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">https://bookstore.ams.org/gsm-104/</a>&quot;,<br>\\n&quot;type&quot;: &quot;book&quot;,<br>\\n&quot;label&quot;: &quot;Alu09&quot;<br>\\n}<br>\\n},<br>\\n{&quot;Tse22&quot;:<br>\\n{<br>\\n&quot;title&quot;: &quot;Topics in Ergodic Theory and Measured Group Theory&quot;,<br>\\n&quot;author&quot;: [&quot;Anush Tserunyan&quot;],<br>\\n&quot;date&quot;: &quot;2022&quot;,<br>\\n&quot;link&quot;: &quot;<a href=\\"https://www.math.mcgill.ca/atserunyan/Courses/2022_W.Math594.Erg&amp;MsGrp/\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">https://www.math.mcgill.ca/atserunyan/Courses/2022_W.Math594.Erg&amp;MsGrp/</a>&quot;,<br>\\n&quot;type&quot;: &quot;online&quot;,<br>\\n&quot;label&quot;: &quot;Tse22&quot;<br>\\n}<br>\\n},<br>\\n{&quot;Tse23&quot;:<br>\\n{<br>\\n&quot;title&quot;: &quot;Advanced Real Analysis 1&quot;,<br>\\n&quot;author&quot;: [&quot;Anush Tserunyan&quot;],<br>\\n&quot;date&quot;: &quot;2023&quot;,<br>\\n&quot;link&quot;: &quot;<a href=\\"https://www.math.mcgill.ca/atserunyan/Courses/2023F.Math564.Analysis%201/\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">https://www.math.mcgill.ca/atserunyan/Courses/2023F.Math564.Analysis 1/</a>&quot;,<br>\\n&quot;type&quot;: &quot;online&quot;,<br>\\n&quot;label&quot;: &quot;Tse23&quot;<br>\\n}<br>\\n},<br>\\n{&quot;Fol99&quot;:<br>\\n{<br>\\n&quot;title&quot;: &quot;Real Analysis, Modern Techniques and Their Applications&quot;,<br>\\n&quot;author&quot;: [&quot;Gerald B. Folland&quot;],<br>\\n&quot;date&quot;: &quot;1999&quot;,<br>\\n&quot;edition&quot;: &quot;2&quot;,<br>\\n&quot;series&quot;: &quot;Pure and Applied Mathematics&quot;,<br>\\n&quot;publisher&quot;: &quot;A Wiley Series of Texts, Monographs and Tracts, John Wiley &amp; Sons&quot;,<br>\\n&quot;link&quot;: &quot;<a href=\\"https://www.wiley.com/en-us/Real+Analysis%3A+Modern+Techniques+and+Their+Applications%2C+2nd+Edition-p-9780471317166\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">https://www.wiley.com/en-us/Real+Analysis%3A+Modern+Techniques+and+Their+Applications%2C+2nd+Edition-p-9780471317166</a>&quot;,<br>\\n&quot;type&quot;: &quot;book&quot;,<br>\\n&quot;label&quot;: &quot;Fol99&quot;<br>\\n}<br>\\n},<br>\\n{&quot;Fur97&quot;:<br>\\n{<br>\\n&quot;title&quot;: &quot;Ergodic behavior of diagonal measures and a theorem of Szemerédi on arithmetic progressions&quot;,<br>\\n&quot;author&quot;: [&quot;Harry Furstenberg&quot;],<br>\\n&quot;date&quot;: &quot;1977&quot;,<br>\\n&quot;journal&quot;: &quot;Journal d’Analyse Mathématique&quot;,<br>\\n&quot;volume&quot;: &quot;31&quot;,<br>\\n&quot;pages&quot;: &quot;204-256&quot;,<br>\\n&quot;type&quot;: &quot;article&quot;,<br>\\n&quot;label&quot;: &quot;Fur97&quot;<br>\\n}<br>\\n}<br>\\n]</p>","autoDesc":true,"bioChainData":{"outlink":[],"backlink":[],"localMap":{"nodes":[{"id":"references.json","value":{"title":"references.json","path":"references.json","outlink":[],"backlink":[]}}],"links":[]}}}');export{h as comp,b as data};
