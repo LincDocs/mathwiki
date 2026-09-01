e8c32138d0a4ea4cadb197bccb1a2257282200ae
LincZero <LincZero@users.noreply.github.com>
2026-09-01 15:01:39 +0000

    Deploying to gh-pages from @ LincDocs/mathwiki@4e11d10462ebafadf05f95e6fd05c8a9c0e18e9c 🚀

---
+++ assets/swimlanesDiagram-JKPDBFCN-C7Kp1wYd.js
@@ -0,0 +1,8 @@
+import{T as t,f as o}from"./flowDiagram-YHGXBVSY-DINX26Mi.js";import"./chunk-AZZRMDJM-B5pQ7yR_.js";import"./chunk-RCXAM2GK-B8W6ZjC_.js";import"./chunk-VTHDQ7BV-CZorIzKy.js";import{m as e}from"./mermaid.esm.min-B9fRTGYs.js";import"./chunk-EICP7K5X-CCI-giAG.js";import"./app-BtD-1zUc.js";var a=e(r=>`${o(r)}
+  .swimlane.cluster rect {
+    stroke: ${r.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=a,u=t({defaultLayout:"swimlane",styles:m});export{u as diagram};
